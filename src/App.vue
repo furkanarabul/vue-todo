6f2301c9d14019d4d912e9b05f67e6d5d8bf9bbf
Furkan <arabulfurkan@gmail.com>
2020-12-26 23:35:22 +0100

    added todoList component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="app" class="container">
+    <img class="logo" src="./assets/logo.png">
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList,
   }
 }
 </script>
 
 <style>
+*{
+  box-sizing: border-box;
+}
+body{
+  font-family: 'Courier New', Courier, monospace;
+  background: rgb(241, 241, 241);
+  color: black;
+  min-height: 100vh;
+  transition: all 0.5s ease;
+}
+input{
+  padding: 1rem;
+  font-size: 1.5rem;
+  border: none;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+.container{
+  max-width: 720px;
+  margin: 0 auto;
+}
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px  ;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,81 @@
+<template>
+<form>
+<div class="header-input">
+    <input type="text" class="todo-input" placeholder="Write a new task"  v-model="newTodo" @keyup.enter="addTodo">
+    <button class="todo-button" type="submit">
+        <i class="fas fa-plus-square"></i>
+    </button>   
+    
+</div>
+<div v-for="todo in todos" :key="todo.id" class="todo-item">
+    {{todo.title}}
+</div>
+</form>
+
+</template>
+
+<script>
+export default {
+    name:'todo-list',
+    data () {
+        return {
+            newTodo: '',
+            todos:[
+                {
+                    'title':'Finish Vue Screencast',
+                },
+                {
+                    'title':'Take over world',
+                },
+            ]
+        }
+    },
+    methods: {
+        addTodo(){
+            this.todos.push({
+                title: this.newTodo,
+            })
+            this.newTodo = ''
+        }
+    }
+}
+</script>
+
+<style>
+.header-input {
+    display: flex;
+    border-radius: 18px;    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+header,
+
+input,
+button {
+    padding: 1rem;
+    font-size: 1.5rem;
+    border: none;
+    font-family: "Abel", sans-serif;
+}
+input {
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+}
+input:focus {
+    outline: none;
+}
+button {
+    color: rgb(65, 65, 65);
+    background: white;
+    cursor: pointer;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    transition: all 0.5s ease;
+    outline: none;
+}
+button:hover {
+    color: #2ca54a;
+}
+</style>
+
